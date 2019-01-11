8bd44ddeeb3ef977c352e7ba8ac79ced5afa813a
Ivan Izdepskyi <iizdepskyy112@gmail.com>
2019-01-11 16:52:26 +0200

    Making use of JdbcDaoSupport using Annotation

--- src/com/izdebski/dao/impl/EmployeeDAOImpl.java
+++ src/com/izdebski/dao/impl/EmployeeDAOImpl.java
@@ -4,23 +4,24 @@ import com.izdebski.dao.EmployeeDAO;
 import com.izdebski.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.util.List;
 
 @Repository
-public class EmployeeDAOImpl implements EmployeeDAO {
-
-    // private DataSource dataSource;
+public class EmployeeDAOImpl extends JdbcDaoSupport implements EmployeeDAO {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    //private JdbcTemplate jdbcTemplate;
+    private DataSource dataSource;
 
     /*public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
         this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }*/
+    }
 
     /*@Override
     public void createEmployee(Employee employee) {
@@ -52,40 +53,45 @@ public class EmployeeDAOImpl implements EmployeeDAO {
                 }
             }
         }
+        */
 
     @Override
     public void createEmployee(Employee employee) {
        /* String SQL = "INSERT INTO employee_table(employee_name, email, gender,salary) VALUES(?,?,?,?)";
         int update = jdbcTemplate.update(SQL, new Object[]{employee.getEmployeeName(),employee.getEmail(),employee.getGender(), employee.getSalary()});
+        */
 
-
-       int update=jdbcTemplate.update("INSERT INTO employee_table(employee_name, email, gender,salary) VALUES(?,?,?,?)", employee.getEmployeeName(),employee.getEmail(),employee.getGender(), employee.getSalary());
+       int update=getJdbcTemplate().update("INSERT INTO employee_table(employee_name, email, gender,salary) VALUES(?,?,?,?)", employee.getEmployeeName(),employee.getEmail(),employee.getGender(), employee.getSalary());
        if(update>0)
             System.out.println("Employee is created ...");
     }
 
+
+
+    /*
+    public void setJdbcTeplate(JdbcTemplate jdbcTemplate){
+    this.jdbcTemplate=jdbcTemplate;
+    }
     */
 
-    @Override // ATTENTION!!!!!!!!!!
-    public void createEmployee(Employee employee) {
-
+    @PostConstruct
+    public void init(){
+        //setJdbcTemplate(jdbcTemplate);
+        setDataSource(dataSource);
     }
 
-    public void setJdbcTeplate(JdbcTemplate jdbcTemplate){
-        this.jdbcTemplate=jdbcTemplate;
-    }
 
     @Override
     public Employee getEmployeeById(int employeeId) {
         String SQL="SELECT*FROM employee_table WHERE employee_id=?";
-        Employee employee = jdbcTemplate.queryForObject(SQL, new EmployeeRowMapper(), employeeId);
+        Employee employee = getJdbcTemplate().queryForObject(SQL, new EmployeeRowMapper(), employeeId);
         return employee;
     }
 
     @Override
     public void deleteEmployeeById(int employeeId) {
         String SQL = "DELETE FROM employee_table WHERE employee_Id=?";
-        int update = jdbcTemplate.update(SQL, employeeId);
+        int update = getJdbcTemplate().update(SQL, employeeId);
         if(update>0)
             System.out.println("Employee is deleted ...");
     }
@@ -93,7 +99,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     @Override
     public void updateEmployeeEmailById(String newEmail, int employeeId) {
         String SQL = "UPDATE employee_table set email=? WHERE employee_Id=?";
-        int update = jdbcTemplate.update(SQL, newEmail, employeeId);
+        int update = getJdbcTemplate().update(SQL, newEmail, employeeId);
         if(update>0)
             System.out.println("Email is updated ...");
     }
@@ -102,6 +108,6 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     public List<Employee> getAllEmployeesDetails() {
         String SQL="SELECT*FROM employee_table";
 
-        return jdbcTemplate.query(SQL, new EmployeeRowMapper());
+        return getJdbcTemplate().query(SQL, new EmployeeRowMapper());
     }
 }
